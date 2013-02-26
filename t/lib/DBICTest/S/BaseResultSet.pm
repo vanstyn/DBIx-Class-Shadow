b78b3dad7a1aae36fd40036c563b9f916b4e83bc
Henry Van Styn <vanstyn@intellitree.com>
2013-02-26 11:01:55 -0500

    Fixed bug in test Rs: populate() calling itself instead of next::method
    populate() in DBICTest::S::BaseResult was trying to return
    shift->populate(@_) which will obviously cause inf recursion. changed to
    shift->next::method(@_)

--- t/lib/DBICTest/S/BaseResultSet.pm
+++ t/lib/DBICTest/S/BaseResultSet.pm
@@ -8,11 +8,11 @@ sub delete { shift->delete_all(@_) };
 
 sub populate {
   if (defined wantarray) {
-    return shift->populate(@_)
+    return shift->next::method(@_)
   }
   else {
     # Force out of void context to avoid Storage::insert_bulk
-    my $res = shift->populate(@_);
+    my $res = shift->next::method(@_);
     return;
   }
 }
